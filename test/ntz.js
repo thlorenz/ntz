03b786c6e31a7e030f082666ec15d958a706ce1c
Thorsten Lorenz <thlorenz@gmx.de>
2018-07-06 23:04:23 -0600

    offset: including GMT as part of offset definition

--- test/ntz.js
+++ test/ntz.js
@@ -4,10 +4,10 @@ const test = require('tape')
 const ntz = require('../')
 
 test('\nreplacing known timezones', function(t) {
-  [ [ 'Sunday, July 24, 19:32:00 CEST 2016', 'Sunday, July 24, 19:32:00 +02 2016' ]
-  , [ 'Sunday, July 24, 19:32:00 CDT 2016', 'Sunday, July 24, 19:32:00 -05 2016' ]
-  , [ 'Mon, 25 Jul 2016 00:32:00 GMT', 'Mon, 25 Jul 2016 00:32:00 +00' ]
-  , [ 'Mon, 25 Jul 2016 00:32:00 THA', 'Mon, 25 Jul 2016 00:32:00 +07' ]
+  [ [ 'Sunday, July 24, 19:32:00 CEST 2016', 'Sunday, July 24, 19:32:00 GMT+02 2016' ]
+  , [ 'Sunday, July 24, 19:32:00 CDT 2016', 'Sunday, July 24, 19:32:00 GMT-05 2016' ]
+  , [ 'Mon, 25 Jul 2016 00:32:00 GMT', 'Mon, 25 Jul 2016 00:32:00 GMT+00' ]
+  , [ 'Mon, 25 Jul 2016 00:32:00 THA', 'Mon, 25 Jul 2016 00:32:00 GMT+07' ]
   ].forEach(check)
 
   function check([ s, expected ]) {
@@ -17,7 +17,7 @@ test('\nreplacing known timezones', function(t) {
   t.end()
 })
 
-test('\nreplacing known timezones returns original string', function(t) {
+test('\nreplacing unknown timezones returns original string', function(t) {
   [ 'Sunday, July 24, 19:32:00 WTF 2016'
   , 'Mon, 25 Jul 2016 00:32:00 THHA'
   ].forEach(check)
